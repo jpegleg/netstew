9ba00cc698a5ce38d8cfb8eb6e921df0e24525cc
Keegan Bowen <keegan.bowen@gmail.com>
2015-11-24 20:53:45 -0700

    Create netstew.py

---
+++ netstew.py
@@ -0,0 +1,9 @@
+#!/opt/anaconda/bin/python2.7
+# Print the links to stndard out.
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(open("index.html"))
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
